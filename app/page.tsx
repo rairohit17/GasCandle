94d377833362fc7731c61a9c4299bb7d0579ae72
Rohit Rai <rohitrai170904@gmail.com>
2025-07-18 06:22:59 +0530

    landing page complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,87 @@
 'use client';
-import Image from 'next/image';
 import Link from 'next/link';
 
+import { useState } from 'react';
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 export default function Home() {
+  const [selectedChain, setSelectedChain] = useState('ethereum');
+  const [mode, setMode] = useState('');
+
   return (
-    <main className="min-h-screen w-full">
-      <div className="text-white"></div>
+    <main className="min-h-screen w-full mt-[30px] px-[30px]">
+      <div className="mt-[100px]">
+        <h2 className="md:max-w-[70vw] stroke text-[2.5rem] text-blue-800 md:text-[3.5rem] transition duration-100 lg:text-[5rem] font-bold cursor-pointer">
+          Know your transaction cost before you send.
+        </h2>
+      </div>
+      <div className="mt-[80px]">
+      <div className="block text-2xl font-medium text-gray-300  text-center mb-[30px]">
+        Select Blockchain
+      </div>
+      <ToggleGroup
+        type="single"
+        variant="outline"
+        value={selectedChain}
+        onValueChange={(val) => {
+          if (val) setSelectedChain(val);
+        }}
+        className="flex flex-wrap justify-center gap-4"
+      >
+        <ToggleGroupItem
+          className={`w-[200px] border-blue-500 hover:text-white hover:bg-blue-500 text-gray-400 data-[state=on]:bg-blue-500 data-[state=on]:text-white`}
+          value="ethereum"
+          aria-label="Toggle Ethereum"
+        >
+          Ethereum
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          className={`w-[200px] border-pink-600 hover:text-white hover:bg-pink-600  text-gray-400 data-[state=on]:bg-pink-600 data-[state=on]:text-white`}
+          value="arbitrum"
+          aria-label="Toggle Arbitrum"
+        >
+          Arbitrum
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          className={`w-[200px] border-purple-700 text-gray-400 hover:bg-purple-700 hover:text-white data-[state=on]:bg-purple-700 data-[state=on]:text-white`}
+          value="polygon"
+          aria-label="Toggle Polygon"
+        >
+          Polygon
+        </ToggleGroupItem>
+      </ToggleGroup>
+
+      <p className="text-center mt-4 text-sm text-gray-400">
+        Selected: <span className="font-semibold">{selectedChain}</span>
+      </p>
+    </div>
+      <div className="mt-[50px]">
+        <h3 className="text-2xl font-semibold mb-[30px] text-gray-300 text-center">
+          Choose Mode
+        </h3>
+        <div className="flex flex-col md:flex-row  md:justify-center  gap-6">
+          <Link href={`/live?chain=${selectedChain}`}>
+            <div className="border border-blue-300 p-6 h-[150px] rounded-xl shadow-md hover:shadow-lg hover:border-blue-500 transition w-full md:w-[300px] cursor-pointer">
+              <h4 className="text-xl font-bold text-blue-700">Live Mode</h4>
+              <p className="text-sm text-gray-600 mt-2">
+                Real-time gas prices & cost preview. Updates every 6s with
+                candle chart every 15min.
+              </p>
+            </div>
+          </Link>
+
+          <Link href={`/simulation?chain=${selectedChain}`}>
+            <div className="border border-purple-300 p-6 rounded-xl h-[150px] mb-[50px] shadow-md hover:shadow-lg hover:border-purple-500 transition w-full md:w-[300px] cursor-pointer">
+              <h4 className="text-xl font-bold text-purple-700">
+                Simulation Mode
+              </h4>
+              <p className="text-sm text-gray-600 mt-2">
+                Simulate a transaction and get cost in USD using current gas
+                data.
+              </p>
+            </div>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -12,24 +12,20 @@ import { Button } from './ui/button';
 const Header = () => {
   return (
     <div className="text-[#2d7ad9] flex justify justify-between mt-[2rem] max-w-full ">
-      <div className="text-3xl ml-[30px]">Logo</div>
+      <div className="text-3xl ml-[30px]">GasEye . </div>
       <div className="mr-[1rem] md:hidden cursor-pointer">
         <Sheet>
-          <SheetTrigger><RxHamburgerMenu></RxHamburgerMenu></SheetTrigger>
-          <SheetContent className='bg-slate-900 text-white  '>
-            <SheetHeader>
-              
-            </SheetHeader>
-              <SheetDescription>
-                 <div className='text-xl space-y-5  '>
-                  <div className='mt-[50px] cursor-pointer'>Home</div>
-                 <div className='mt-[20px] cursor-pointer'>About</div></div>
-              
-              </SheetDescription>
-               
-                
-                
-              
+          <SheetTrigger>
+            <RxHamburgerMenu></RxHamburgerMenu>
+          </SheetTrigger>
+          <SheetContent className="bg-slate-900 text-white  ">
+            <SheetHeader></SheetHeader>
+            <SheetDescription>
+              <div className="text-xl space-y-5  ">
+                <div className="mt-[50px] cursor-pointer">Home</div>
+                <div className="mt-[20px] cursor-pointer">About</div>
+              </div>
+            </SheetDescription>
           </SheetContent>
         </Sheet>
       </div>
